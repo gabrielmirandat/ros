ac7c5a371d985fa2f6ae8b69dadd3ad62eb1f790
gabrielmirandat <gabrielmirandatt@gmail.com>
2017-07-01 18:41:12 -0300

    Removed not needed files

--- packages_ws/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/gabriel/ros/packages_ws/src
-# Build directory: /home/gabriel/ros/packages_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-SUBDIRS(gtest)
-SUBDIRS(rosR)
--- packages_ws/build/rosR/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/gabriel/ros/packages_ws/src/rosR
-# Build directory: /home/gabriel/ros/packages_ws/build/rosR
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
