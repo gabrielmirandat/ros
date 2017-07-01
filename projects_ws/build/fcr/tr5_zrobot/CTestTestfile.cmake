8701ec92fd4b46052537d6613b8d7475d3e71d4a
gabrielmirandat <gabrielmirandatt@gmail.com>
2017-07-01 18:25:44 -0300

    Updated gitignore

--- projects_ws/build/fcr/tr5_zrobot/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/gabriel/ros/projects_ws/src/fcr/tr5_zrobot
-# Build directory: /home/gabriel/ros/projects_ws/build/fcr/tr5_zrobot
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- projects_ws/build/fcr/tr_final_robot/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/gabriel/ros/projects_ws/src/fcr/tr_final_robot
-# Build directory: /home/gabriel/ros/projects_ws/build/fcr/tr_final_robot
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
